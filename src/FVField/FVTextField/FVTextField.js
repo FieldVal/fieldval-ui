446f5fc9f1fe464bec9644f614a175361a566670
Marcus Longmuir <marcuslongmuir@me.com>
2015-04-09 19:05:17 +0300

    Switched tab insertion method to document.executeCommand

--- src/FVField/FVTextField/FVTextField.js
+++ src/FVField/FVTextField/FVTextField.js
@@ -47,17 +47,9 @@ function FVTextField(name, options) {
             }
         }
 
-        if(field.consume_tabs && e.keyCode===9) {
+        if(field.input_type==="textarea" && field.consume_tabs && e.keyCode===9) {
             e.preventDefault();
-            var start = $(this).get(0).selectionStart;
-            var end = $(this).get(0).selectionEnd;
-
-            // set textarea value to: text before caret + tab + text after caret
-            $(this).val($(this).val().substring(0, start) + "\t" + $(this).val().substring(end));
-
-            // put caret at right position again
-            $(this).get(0).selectionStart =
-            $(this).get(0).selectionEnd = start + 1;
+            document.execCommand("insertText", false, "\t");
         }
     })
     .on("keyup paste cut",function(){
